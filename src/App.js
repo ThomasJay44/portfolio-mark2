e20449f3cbb7d6753173c7eadda606e9284b4dae
tommysoprano <thomasjaymaly@gmail.com>
2023-05-23 20:09:31 -0400

    more structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-
 import './App.css';
+import {useState } from "react";
+import { Header, Footer, Nav } from "./Components";
+import {Home, Contact, Activities, Projects}  from "./Pages/Home";
+
 
 export default function App() {
+
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+      if (currentPage === "Home") return <Home />
+      if (currentPage === "Contact") return <Contact />
+      if (currentPage === "Activities") return <Activities />
+      if (currentPage === "Projects") return <Projects />
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+      <Nav currentPage={ currentPage } handlePageChange={handlePageChange}     />
+      {renderPage(currentPage)}
+      <Home />
+      <Footer />
     </div>
   );
-}
+  }
+
 
 
---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome To my portfolio</h1>
+    </header>
+  );
+}
