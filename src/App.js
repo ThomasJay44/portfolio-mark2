fbee3469e0796c510a4ab17cc0fe26b53a505e73
tommysoprano <thomasjaymaly@gmail.com>
2023-05-23 20:16:56 -0400

    fixing things

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
-import {useState } from "react";
+import "./App.css";
+import { useState } from "react";
 import { Header, Footer, Nav } from "./Components";
-import {Home, Contact, Activities, Projects}  from "./Pages/Home";
-
+import { Home, Contact, Activities, Projects } from "./Pages";
 
 export default function App() {
-
   const [currentPage, setCurrentPage] = useState("Home");
 
   const renderPage = () => {
-      if (currentPage === "Home") return <Home />
-      if (currentPage === "Contact") return <Contact />
-      if (currentPage === "Activities") return <Activities />
-      if (currentPage === "Projects") return <Projects />
+    if (currentPage === "Home") return <Home />;
+    if (currentPage === "Contact") return <Contact />;
+    if (currentPage === "Activities") return <Activities />;
+    if (currentPage === "Projects") return <Projects />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
@@ -20,13 +18,10 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <Nav currentPage={ currentPage } handlePageChange={handlePageChange}     />
-      {renderPage(currentPage)}
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* {renderPage(currentPage)} */}
       <Home />
       <Footer />
     </div>
   );
-  }
-
-
-
+}
