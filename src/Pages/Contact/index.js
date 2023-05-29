34ed09b6d50039a935d43854bd37dbe32444ffab
tommysoprano <thomasjaymaly@gmail.com>
2023-05-28 20:50:45 -0400

    adding social links

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -3,8 +3,15 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faTwitter,
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+import "./style.css";
 
 export default function Contact() {
   const handleEmailClick = () => {
@@ -17,25 +24,53 @@ export default function Contact() {
 
       <Row className="mb-3">
         <Col xs={6} sm={3} className="d-flex justify-content-center">
-          <Button variant="twitter" href="https://twitter.com/your_twitter_username" className="flex-fill">
+          <Button
+            variant="twitter"
+            href="https://twitter.com/your_twitter_username"
+            className="flex-fill"
+          >
             <FontAwesomeIcon icon={faTwitter} size="3x" />
           </Button>
         </Col>
 
         <Col xs={6} sm={3} className="d-flex justify-content-center">
-          <Button variant="instagram" href="https://instagram.com/your_instagram_username" className="flex-fill">
+          <Button
+            variant="instagram"
+            href="https://instagram.com/your_instagram_username"
+            className="flex-fill"
+          >
             <FontAwesomeIcon icon={faInstagram} size="3x" />
           </Button>
         </Col>
 
         <Col xs={6} sm={3} className="d-flex justify-content-center">
-          <Button variant="github" href="https://github.com/your_github_username" className="flex-fill">
+          <Button
+            variant="github"
+            href="https://github.com/ThomasJay44"
+            className="flex-fill"
+          >
             <FontAwesomeIcon icon={faGithub} size="3x" />
           </Button>
         </Col>
 
         <Col xs={6} sm={3} className="d-flex justify-content-center">
-          <Button variant="email" onClick={handleEmailClick} className="flex-fill">
+          <Button
+            variant="linkedin"
+            href="https://www.linkedin.com/in/thomas-maly-29825189/"
+            className="flex-fill"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="3x" />
+          </Button>
+        </Col>
+      </Row>
+
+      <Row className="mb-3">
+        <Col className="d-flex justify-content-center">
+          <Button
+            variant="email"
+            onClick={handleEmailClick}
+            className="flex-fill"
+          >
             <FontAwesomeIcon icon={faEnvelope} size="3x" />
           </Button>
         </Col>
